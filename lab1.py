93b00b6a3141702ef5f8f8f3cd0c5ecea2ae4cc9
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-03-31 18:49:48 +0300

    v0.9.1 modify so unittest can do its work

--- lab1.py
+++ lab1.py
@@ -8,32 +8,56 @@
 
 import sys
 
-text_csv = sys.stdin.readlines()
-is_first_line = True
-full_text = []
-for line in text_csv:
-    line = line[:-1]
-    if is_first_line:
-        line_first = line.split(',')
-        is_first_line = False
+
+def transorm_from_csv_to_json(csv_from_test=None, show_stdout=True, receive_stdout=False, show_stderr=True): #, receive_stderr=False
+    if csv_from_test is None:
+        text_csv = sys.stdin.readlines()
     else:
-        line = line.split(',')
-        for item_index in range(len(line)):
-            try:
-                line[item_index] = float(line[item_index])
-            except TypeError and ValueError:
-                pass
-        line_text = ''
-        for name_index in range(len(line_first)-1):
-            line_text += [f'"{line_first[name_index]}": "{line[name_index]}", ', f'"{line_first[name_index]}": {line[name_index]}, '][type(line[name_index]) is float]
-        line_text += [f'"{line_first[-1]}": "{line[-1]}"', f'"{line_first[-1]}": {line[-1]}'][type(line[-1]) is float]
-        full_text += [{line_text}]
+        show_stdout = False
+        show_stderr = False
+        text_csv = csv_from_test
+    # print(text_csv)
+    is_first_line = True
+    full_text = []
+    exit_number = 0
 
-json_text = str(full_text)
-json_text = "".join(json_text.split("'"))
-print(json_text)
+    for line in text_csv:
+        line_index = text_csv.index(line)
+        line = line[:-1]
+        if is_first_line:
+            line_first = line.split(',')
+            is_first_line = False
+        else:
+            line = line.split(',')
+            if len(line) != len(line_first):
+                if show_stderr:
+                    sys.stderr.write(f"csv file on row {line_index+1} are not full\n")
+                exit_number = 1
+            else:
+                for item_index in range(len(line)):
+                    try:
+                        if "." in line[item_index]:
+                            line[item_index] = float(line[item_index])
+                        else:
+                            line[item_index] = int(line[item_index])
+                    except ValueError:
+                        pass
+                        # sys.stderr.write(f"Cannot convert {line[item_index]} to float because value is wrong\n")
+                line_text = ''
+                for name_index in range(len(line_first)-1):
+                    line_text += [f'"{line_first[name_index]}": "{line[name_index]}", ', f'"{line_first[name_index]}": {line[name_index]}, '][type(line[name_index]) is float or type(line[name_index]) is int]
+                line_text += [f'"{line_first[name_index+1]}": "{line[name_index+1]}"', f'"{line_first[name_index+1]}": {line[name_index+1]}'][type(line[name_index+1]) is float or type(line[name_index+1]) is int]
+                full_text += [{line_text}]
 
-sys.exit(0)
+    json_text = str(full_text)
+    json_text = "".join(json_text.split("'"))
+    if show_stdout:
+        print(json_text)
+    if receive_stdout:
+        return exit_number, json_text
+    return exit_number
 
-# with open("test.txt", "w") as jsn:
-#     jsn.write(json_text)
+
+if __name__ == '__main__':
+    exit_number = transorm_from_csv_to_json()
+    sys.exit(exit_number)
