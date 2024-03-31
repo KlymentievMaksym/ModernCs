3825230c8e8bfbd24f7dbd517e6f7938f540d780
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-03-31 15:40:30 +0300

    v0.9.0 requirenments 1, 3 and 4 done

--- lab1.py
+++ lab1.py
@@ -1,14 +1,16 @@
+"""Вимоги:
+- реалізувати завдання згідно до вашого номеру в групі (якщо ваш номер 16 або 17, то відповідно берете 1 або 2)
+- обов'язкове покриття програми юніт-тестами
+- мова програмування на ваш смак
+- якщо програма виконалась успішно, вона має повернути код виходу 0
+- будь-які помилки в програмі мають писатися в stderr, а сама програма при цьому повертати будь-який ненульовий код виходу
+"""
+
 import sys
-import json
-
-from io import StringIO
-
-io = StringIO()
-result = StringIO()
 
 text_csv = sys.stdin.readlines()
 is_first_line = True
-print()
+full_text = []
 for line in text_csv:
     line = line[:-1]
     if is_first_line:
@@ -19,17 +21,19 @@ for line in text_csv:
         for item_index in range(len(line)):
             try:
                 line[item_index] = float(line[item_index])
-            except TypeError:
+            except TypeError and ValueError:
                 pass
-        # print(line)
-        print(*line_first, *line)
-        for name_index in range(len(line_first)):
-            json.dump({line_first[name_index]: line[name_index]}, io)
-        print(io.getvalue())
-        break
-json.dump([io.getvalue()], result)
-print(result.getvalue())
-print()
+        line_text = ''
+        for name_index in range(len(line_first)-1):
+            line_text += [f'"{line_first[name_index]}": "{line[name_index]}", ', f'"{line_first[name_index]}": {line[name_index]}, '][type(line[name_index]) is float]
+        line_text += [f'"{line_first[-1]}": "{line[-1]}"', f'"{line_first[-1]}": {line[-1]}'][type(line[-1]) is float]
+        full_text += [{line_text}]
 
-# name,age\nJohn,30\nJane,25
-# [{ "name": "John", "age": 30 }, { "name": "Jane", "age": 25 }]
+json_text = str(full_text)
+json_text = "".join(json_text.split("'"))
+print(json_text)
+
+sys.exit(0)
+
+# with open("test.txt", "w") as jsn:
+#     jsn.write(json_text)
